2b78b2e6f46ae9c74d6be69a1398508a0aee7c12
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2025-07-16 22:42:01 +0530

    Update jenkins-worker-automated-installation.sh

--- scripts/jenkins-worker-automated-installation.sh
+++ scripts/jenkins-worker-automated-installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
 apt-get -y upgrade
-apt-get -y install openjdk-21-jdk docker-ce docker-ce-cli containerd.io
+apt-get -y install openjdk-21-jdk docker-ce docker-ce-cli containerd.io git-all
 systemctl enable docker.service
 systemctl enable containerd.service
 usermod -aG docker ubuntu
