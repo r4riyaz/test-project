ba821fbd983b746d829e1641908c509816257201
Riyaz Qureshi <42235756+r4riyaz@users.noreply.github.com>
2025-07-18 01:14:54 +0530

    Update jenkins-worker.sh

--- terraform/jenkins-worker.sh
+++ terraform/jenkins-worker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get -y upgrade
 apt-get -y install openjdk-21-jdk docker-ce docker-ce-cli containerd.io git-all
